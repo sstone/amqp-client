01d0bcb1d245162f428e12c247cf4f160b36683b
pm47 <spam4420@gmail.com>
2012-11-13 15:59:45 +0100

    added basic unitary test

--- src/test/scala/com/aphelia/amqp/ConnectionOwnerSpec.scala
+++ src/test/scala/com/aphelia/amqp/ConnectionOwnerSpec.scala
@@ -2,13 +2,25 @@ package com.aphelia.amqp
 
 import akka.testkit.TestProbe
 import akka.actor.Props
-import akka.util.Duration
 import akka.util.duration._
 import com.aphelia.amqp.ConnectionOwner.CreateChannel
 import com.rabbitmq.client.Channel
 import com.aphelia.amqp.Amqp._
 
 class ConnectionOwnerSpec extends BasicAmqpTestSpec {
+
+
+  "ConnectionOwner" should {
+    "provide a working userfriendly constructor" in {
+      val conn = system.actorOf(Props(new ConnectionOwner(vhost = "/")), name = "connection")
+      waitForConnection(system, conn).await()
+      val p = TestProbe()
+      p.send(conn, CreateChannel)
+      p.expectMsgClass(2 second, classOf[Channel])
+      system stop conn
+    }
+  }
+
   "ConnectionOwner" should {
     "provide channels for many child actors" in {
       val conn = system.actorOf(Props(new ConnectionOwner(connFactory)), name = "connection-owner")
