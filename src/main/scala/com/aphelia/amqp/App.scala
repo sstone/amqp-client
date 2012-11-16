29906965dff630a24b2130cfb29c45d824df0ef4
Jan Machacek <janm@cakesolutions.net>
2012-11-16 10:30:43 +0000

    Updated the codebase against Scala 2.10, Akka 2.1 and Specs2

--- src/main/scala/com/aphelia/amqp/App.scala
+++ src/main/scala/com/aphelia/amqp/App.scala
@@ -1,9 +1,9 @@
 package com.aphelia.amqp
 
+import scala.language.postfixOps
+
 import akka.pattern.ask
-import akka.util.duration._
 import com.rabbitmq.client.ConnectionFactory
-import akka.dispatch.Await
 import com.aphelia.amqp.RpcClient.{Response, Request}
 import akka.actor._
 import akka.actor.FSM.{Transition, SubscribeTransitionCallBack}
@@ -13,7 +13,9 @@ import akka.util.Timeout
 import com.rabbitmq.client.AMQP.{BasicProperties, Queue}
 import akka.actor.Status.Failure
 import com.aphelia.amqp.RpcServer.ProcessResult
-
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 
 object App {
 
@@ -26,7 +28,7 @@ object App {
     waitForConnection(system, conn).await()
     val c = ConnectionOwner.createActor(conn, Props(new ChannelOwner()))
     waitForConnection(system, c).await()
-    implicit val timeout = Timeout(2 seconds)
+    implicit val timeout = Timeout(2.seconds)
     val check = Await.result(c.ask(DeclareQueue(QueueParameters("no_such_queue", passive = true))), timeout.duration)
     check match {
       case Amqp.Error(_, cause) => throw cause
@@ -56,7 +58,7 @@ object App {
       }
     }))
     // create a consumer that will pass all messages to the foo Actor; the consumer will declare the bindings
-    val consumer = ConnectionOwner.createActor(conn, Props(new Consumer(List(Binding(exchange, queue, "my_key", autoack = false)), foo)), 5000 millis)
+    val consumer = ConnectionOwner.createActor(conn, Props(new Consumer(List(Binding(exchange, queue, "my_key", autoack = false)), foo)), 5000.millis)
     val producer = ConnectionOwner.createActor(conn, Props(new ChannelOwner()))
     waitForConnection(system, consumer, producer).await()
     producer ! Publish("amq.direct", "my_key", "yo!".getBytes, Some(new BasicProperties.Builder().contentType("my content").build()))
@@ -89,7 +91,7 @@ object App {
       }
     }))
     // create a consumer that will pass all messages to the foo Actor; the consumer will declare the bindings
-    val consumer = ConnectionOwner.createActor(conn, Props(new Consumer(List(Binding(exchange, queue, "my_key", autoack = false)), foo)), 5000 millis)
+    val consumer = ConnectionOwner.createActor(conn, Props(new Consumer(List(Binding(exchange, queue, "my_key", autoack = false)), foo)), 5000.millis)
     val producer = ConnectionOwner.createActor(conn, Props(new ChannelOwner()))
     waitForConnection(system, consumer, producer).await()
     producer ! Publish("amq.direct", "my_key", "yo!".getBytes)
@@ -149,15 +151,15 @@ object App {
     // which means that the broker will generate a unique, random name when the queue is declared
     val queue = QueueParameters(name = "", passive = false, exclusive = true)
     // create 2 servers, each with its own private queue bound to the same key
-    val server1 = ConnectionOwner.createActor(conn, Props(new RpcServer(queue, exchange, "my_key", proc)), 2000 millis)
-    val server2 = ConnectionOwner.createActor(conn, Props(new RpcServer(queue, exchange, "my_key", proc)), 2000 millis)
-    val client = ConnectionOwner.createActor(conn, Props(new RpcClient()), 2000 millis)
+    val server1 = ConnectionOwner.createActor(conn, Props(new RpcServer(queue, exchange, "my_key", proc)), 2000.millis)
+    val server2 = ConnectionOwner.createActor(conn, Props(new RpcServer(queue, exchange, "my_key", proc)), 2000.millis)
+    val client = ConnectionOwner.createActor(conn, Props(new RpcClient()), 2000.millis)
     waitForConnection(system, server1, server2, client).await()
     for (i <- 0 to 10) {
       try {
         // send one request and wait for 2 responses
-        val future = client.ask(Request(Publish("amq.direct", "my_key", "client1".getBytes) :: Nil, 2))(1000 millis)
-        val result = Await.result(future, 1000 millis).asInstanceOf[Response]
+        val future = client.ask(Request(Publish("amq.direct", "my_key", "client1".getBytes) :: Nil, 2))(1000.millis)
+        val result = Await.result(future, 1000.millis).asInstanceOf[Response]
         println("result : " + result)
         Thread.sleep(100)
       }
@@ -215,17 +217,17 @@ object App {
           ExchangeParameters(exchange, passive = true, exchangeType = "direct"),
           key,
           proc)
-        ), 2000 millis
+        ), 2000.millis
       )
       while (true) {
         Thread.sleep(100)
       }
     }
     else {
-      val client = ConnectionOwner.createActor(conn, Props(new RpcClient()), 2000 millis)
+      val client = ConnectionOwner.createActor(conn, Props(new RpcClient()), 2000.millis)
       waitForConnection(system, client)
-      val future = client.ask(Request(Publish(exchange, key, message.getBytes) :: Nil, numberOfResponse))(1000 millis)
-      val result = Await.result(future, 1000 millis).asInstanceOf[Response]
+      val future = client.ask(Request(Publish(exchange, key, message.getBytes) :: Nil, numberOfResponse))(1000.millis)
+      val result = Await.result(future, 1000.millis).asInstanceOf[Response]
       println("result " + result)
     }
     system.shutdown()
